ffde51fc9cbb6e6bb0d271e59a43e38ec852bcb3
Sebas <jseb.patino@gmail.com>
2025-11-21 20:54:52 -0500

    segundo ejercicio resuelto

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-function registrarUsuarios(usuarioPrincipal, ...usuariosAdicionales) {
-   const { nombre, edad } = usuarioPrincipal;
+// --------------------------------------------
+// EJERCICIO 1
+// --------------------------------------------
 
-   console.log(
-    `Usuario principal: ${nombre}, Edad: ${edad} - Usuarios adicionales registrados: ${usuariosAdicionales.length}`)
+// function registrarUsuarios(usuarioPrincipal, ...usuariosAdicionales) {
+//    const { nombre, edad } = usuarioPrincipal;
+
+//    console.log(
+//     `Usuario principal: ${nombre}, Edad: ${edad} - Usuarios adicionales registrados: ${usuariosAdicionales.length}`)
+// }
+
+// registrarUsuarios(
+//   { nombre: "Juan", edad: 22 },
+//   { nombre: "Sebas", edad: 20 },
+//   { nombre: "Karol", edad: 18 },
+//   { nombre: "Nicolle", edad: 19 } 
+// );
+
+// --------------------------------------------
+// EJERCICIO 2
+// --------------------------------------------
+
+const productos = ["teclado", "mouse", "pantalla"];
+
+function agregarProducto(lista, nuevoProducto) {
+    const nuevaLista = [...lista, nuevoProducto];
+    return nuevaLista;    
 }
 
-registrarUsuarios(
-  { nombre: "Juan", edad: 22 },
-  { nombre: "Sebas", edad: 20 },
-  { nombre: "Karol", edad: 18 },
-  { nombre: "Nicolle", edad: 19 } 
-);
+const productosActualizados = agregarProducto(productos, "cpu");
+console.log("Lista original:", productos);
+console.log("Lista actualizada:", productosActualizados);
